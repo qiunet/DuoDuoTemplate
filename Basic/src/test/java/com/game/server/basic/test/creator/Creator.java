8eb995903c64d537bf5a99a349b6cae25fa7934f
qiunet <cLAsj8.mU2cgJ&P>
2020-07-31 12:04:17 +0800

    Creator 放到Basic的test里面去. 之后可以扩展不需要引用Basic但是也需要连库的module

---
+++ Basic/src/test/java/com/game/server/basic/test/creator/Creator.java
@@ -0,0 +1,16 @@
+package com.game.server.basic.test.creator;
+
+import org.qiunet.project.init.creator.ProjectInitCreator;
+
+/***
+ *
+ *
+ * qiunet
+ * 2019-08-14 18:05
+ ***/
+public class Creator {
+
+	public static void main(String[] args) {
+		ProjectInitCreator.create("xml");
+	}
+}
